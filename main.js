5615565aecd7c6d8f7a9ad557fc3ffd312e4838d
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-12-03 11:16:27 +0100

    challenge2_odd even

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Challenge 1
 function LongestWord(sen) {
   // separate word, split(" ")
   const longest = sen
@@ -8,3 +9,16 @@ function LongestWord(sen) {
 }
 
 console.log(LongestWord('fun&!! time jkjkjljljlj'));
+
+// Challenge 2
+function pickIt(arr) {
+  const odd = [],
+    even = [];
+  //coding here
+  arr.map((num) => (num % 2 === 0 ? even.push(num) : odd.push(num)));
+
+  return [odd, even];
+}
+
+const array = [4, 6, 8, 9, 10, 3, 6, 7];
+console.log(pickIt(array));
