d55b6223a30ace5226f620f55244580305de1560
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-12-03 11:19:01 +0100

    ch4_givemefive

--- main.js
+++ main.js
@@ -39,3 +39,21 @@ const howManydays = function (month) {
 };
 
 console.log(howManydays(5));
+
+// Challenge 4
+// Coding in function giveMeFive, function accept 1 parameter:obj, it's an object.
+// You need to the traverse the obj, if the length of the object key equals to 5, then push the key value to the array (you need to define the array by yourself, this time I won't help you). Additionally push the value to the array as well, if the length of the value is equal to 5.
+
+const giveMeFive = function (obj) {
+  const five = [];
+  const objToArr = Object.entries(obj)
+    .filter((str) => str[1].length === 5)
+    .map((str) => five.push(str[0]));
+  return five;
+};
+
+const five = [];
+
+// Testing
+const obj = { Ihave: 'enough', money: 'to', buy: 'a', car: 'model' };
+console.log(giveMeFive(obj));
