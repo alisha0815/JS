58167c9a4bf6c607e428240cf4946922afa46efb
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-12-03 13:46:24 +0100

    infinite loop_pop,shift,push,unshift

--- main.js
+++ main.js
@@ -129,3 +129,48 @@ const alienLanguage = function (str) {
     )
     .join(' ');
 };
+
+// challenge 9
+const infiniteLoop = function (arr, d, n) {
+  for (let i = 1; i <= n; i++) {
+    if (d === 'left') {
+      arr[2].push(arr[0].shift());
+      arr[1].push(arr[2].shift());
+      arr[0].push(arr[1].shift());
+    }
+    if (d === 'right') {
+      arr[0].unshift(arr[2].pop());
+      arr[1].unshift(arr[0].pop());
+      arr[2].unshift(arr[1].pop());
+    }
+  }
+  return arr;
+};
+
+infiniteLoop(
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ],
+  'left',
+  1
+);
+infiniteLoop(
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ],
+  'right',
+  1
+);
+infiniteLoop(
+  [
+    [1, 2],
+    [3, 4, 5, 6],
+    [7, 8, 9, 10],
+  ],
+  'left',
+  2
+);
